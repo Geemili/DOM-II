b474475664e222b6509d5f2417c8887b77c88c19
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-11 14:01:05 -0700

    Add a drag and drop handler to bottom text section

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 // Your code goes here
 
+let contentPickNextId = 0;
 
 function main() {
     document.querySelectorAll("img").forEach(img => {
@@ -11,19 +12,47 @@ function main() {
         });
     });
 
+    // Set up content pick as a drop target
+    const contentPick = document.querySelector(".content-pick");
+    contentPick.id = "content-pick-" + contentPickNextId++;
+    contentPick.addEventListener("dragover", ev => {
+        if (ev.currentTarget != ev.target) return;
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = "move";
+        ev.target.style.backgroundColor = "red";
+    });
+    contentPick.addEventListener("dragleave", ev => {
+        if (ev.currentTarget != ev.target) return;
+        ev.target.style.removeProperty("background-color");
+    });
+    contentPick.addEventListener("drop", ev => {
+        if (ev.currentTarget != ev.target) return;
+        ev.target.style.removeProperty("background-color");
+
+        const draggedElRef = ev.dataTransfer.getData("application/fun-bus");
+        const draggedEl = document.querySelector(draggedElRef);
+        ev.target.appendChild(draggedEl);
+    });
+
+    // Clone content pick!
     document.addEventListener("keydown", event => {
         if (event.key == "n") {
             const pickNode = document.querySelector(".content-pick");
             const cloned = pickNode.cloneNode(true);
+            cloned.id = "content-pick-" + contentPickNextId++;
             document.querySelector(".container.home").appendChild(cloned);
         }
     });
 
+    // Set up destinations as draggable
+    let nextDestId = 0;
     document.querySelectorAll(".content-pick .destination").forEach(el => {
         el.setAttribute("draggable", true);
+        const uniqClass = `${el.parentElement.id}-dest-${nextDestId++}`;
+        el.classList.add(uniqClass);
         el.addEventListener("dragstart", ev => {
-            ev.dataTransfer.setData("application/fun-bus", ev.target.id);
-            console.log(ev.target.id);
+            const uniqRef = `.${uniqClass}`;
+            ev.dataTransfer.setData("application/fun-bus", uniqRef);
             ev.dataTransfer.dropEffect = "move";
         });
     });
