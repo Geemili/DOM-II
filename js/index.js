0e3759878e0719499589031918e103461dc7df2b
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-11 09:30:57 -0700

    Make images turn slightly transparent when hovered over

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+function main() {
+    document.querySelectorAll("img").forEach(img => {
+        img.addEventListener("mouseenter", event => {
+            event.target.style.opacity = "0.5";
+        });
+        img.addEventListener("mouseleave", event => {
+            event.target.style.opacity = "1.0";
+        });
+    });
+}
+
+window.onload = main;
