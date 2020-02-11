07ffbe625bc8760e7832a0017aba849c5a5ecdce
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-02-11 15:24:17 -0700

    Make rockets move to the right when clicked

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,11 +1,26 @@
 
 function main() {
     const mainContainer = document.querySelector(".blocks");
+
     mainContainer.addEventListener("click", ev => {
         if (ev.target.classList.contains("block")) {
             ev.target.remove();
             mainContainer.prepend(ev.target);
         }
+    });
+
+    let mouseDownTimer = undefined;
+    mainContainer.addEventListener("mousedown", ev => {
+        if (ev.target.classList.contains("block")) {
+            mouseDownTimer = setInterval(() => {
+                const curMargin = parseInt(window.getComputedStyle(ev.target).marginLeft);
+                ev.target.style.marginLeft = `${curMargin + 5}px`;
+            }, 20);
+        }
+    });
+
+    mainContainer.addEventListener("mouseup", ev => {
+        clearInterval(mouseDownTimer);
     });
 }
 
